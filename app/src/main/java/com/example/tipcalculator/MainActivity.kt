32cb008100e53ba9d122d5cdcc7624d484cdc859
Natalia <NSzymczak>
2022-07-19 15:39:05 +0200

    Tips works (without warnings)

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.example.tipcalculator
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.example.tipcalculator.databinding.ActivityMainBinding
-import java.lang.Math.ceil
 import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
@@ -25,8 +24,7 @@ class MainActivity : AppCompatActivity() {
             binding.tipResult.text=""
             return
         }
-        val selectedId = binding.tipOptions.checkedRadioButtonId
-        val tipPercentage: Double = when (selectedId) {
+        val tipPercentage: Double = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
@@ -34,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         var tip = tipPercentage*cost
         val roundUp = binding.roundUpSwitch.isChecked
         if(roundUp) {
-            tip = ceil(tip)
+            tip = kotlin.math.ceil(tip)
         }
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount,formattedTip)
