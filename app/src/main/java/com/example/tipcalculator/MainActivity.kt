4925f48f87c74552f7666861a4f695f6dab8c39c
Natalia <NSzymczak>
2022-07-19 15:38:23 +0200

    Tips works

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -3,14 +3,42 @@ package com.example.tipcalculator
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tipcalculator.databinding.ActivityMainBinding
+import java.lang.Math.ceil
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.buttonCalculate.setOnClickListener{calculateTip()}
     }
+
+    private fun calculateTip(){
+
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if(cost==null){
+            binding.tipResult.text=""
+            return
+        }
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage: Double = when (selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        var tip = tipPercentage*cost
+        val roundUp = binding.roundUpSwitch.isChecked
+        if(roundUp) {
+            tip = ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount,formattedTip)
+    }
+
+
 }
